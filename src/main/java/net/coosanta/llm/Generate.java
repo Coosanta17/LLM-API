2e0576cc67d319ad15d10f933685628c80d26848
Coosanta <coosantamapy@gmail.com>
2024-11-09 18:59:49 +1100

    Removed unused imports

--- src/main/java/net/coosanta/llm/Generate.java
+++ src/main/java/net/coosanta/llm/Generate.java
@@ -1,15 +1,8 @@
 package net.coosanta.llm;
 
-import de.kherud.llama.InferenceParameters;
-import de.kherud.llama.LlamaModel;
-import de.kherud.llama.LlamaOutput;
 import de.kherud.llama.ModelParameters;
-import de.kherud.llama.args.MiroStat;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 
 import static net.coosanta.llm.Main.MODEL_PATH;
 
