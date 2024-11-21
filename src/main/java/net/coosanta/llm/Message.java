aa95dd49cbb0ea61cc5d4397d779df1d9ef71433
Coosanta <coosantamapy@gmail.com>
2024-11-21 18:28:01 +1100

    Removed commented out code

--- src/main/java/net/coosanta/llm/Message.java
+++ src/main/java/net/coosanta/llm/Message.java
@@ -1,23 +1,3 @@
 package net.coosanta.llm;
 
 public record Message(String role, String content) {}
-
-/*
-public class Message {
-    private final String role;
-    private final String content;
-
-    public Message(String role, String content) {
-        this.role = role;
-        this.content = content;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public String getContent() {
-        return content;
-    }
-}
-*/
--- src/main/java/net/coosanta/llm/controller/LlmController.java
+++ src/main/java/net/coosanta/llm/controller/LlmController.java
@@ -15,21 +15,6 @@ public class LlmController {
         this.llamaConfig = llamaConfig;
     }
 
-//    @GetMapping("/getSomething")
-//    public String getSomething() {
-//        return "something";
-//    }
-
-//    // curl -H "Content-Type: application/json" -d '{"prompt": "Hello world!"}' http://localhost:port/api/testPrompt
-//    // You sent: Hello world!
-//    @PostMapping("/testPrompt")
-//    public ResponseEntity<String> processPrompt(@RequestBody Prompt prompt) {
-//        Responder response = new Responder(prompt);
-//
-//        String result = response.respond();
-//        return ResponseEntity.ok(result);
-//    }
-
     // PowerShell: Invoke-RestMethod -Uri "http://localhost:8080/api/generate" -Method Post -Headers @{ "Content-Type" = "application/json" } -Body '"You are a helpful and knowledgeable assistant."'
     // Bash: curl -H "Content-Type: application/json" -d 'You are a helpful and knowledgeable assistant.' http://localhost:8080/api/generate
     @PostMapping("/generate")
