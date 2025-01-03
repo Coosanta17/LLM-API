f4f5633c4387bcac6d29b910179c3029bebf3ec0
Coosanta <coosantamapy@gmail.com>
2025-01-03 14:42:09 +1100

    Improved Cross-Origin Resource Sharing configuration

--- src/main/java/net/coosanta/llm/CorsConfig.java
+++ src/main/java/net/coosanta/llm/CorsConfig.java
@@ -1,8 +1,11 @@
 package net.coosanta.llm;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -33,6 +36,26 @@ public class CorsConfig {
                         .allowCredentials(allowCredentials);
             }
         };
+    }
+
+    @Bean
+    public DispatcherServletBeanPostProcessor dispatcherServletBeanPostProcessor() {
+        return new DispatcherServletBeanPostProcessor();
+    }
+
+    public static class DispatcherServletBeanPostProcessor implements BeanPostProcessor {
+        @Override
+        public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+            if (bean instanceof DispatcherServlet) {
+                ((DispatcherServlet) bean).setDispatchOptionsRequest(true);
+            }
+            return bean;
+        }
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            return bean;
+        }
     }
 }
 
